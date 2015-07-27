4eccc83686f67b6d68455e72a0b59596b41114dd
Jonathan Guerrero <jonathan@practisis.com>
2015-07-27 16:30:21 -0500

    primera prueba del scanner

---
+++ www/js/indexscan.js
@@ -0,0 +1,16 @@
+
+function dale(){
+alert("vamos");
+cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+   );
+   
+}
